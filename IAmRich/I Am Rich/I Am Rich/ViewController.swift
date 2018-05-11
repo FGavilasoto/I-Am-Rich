7282e552524327480c7b21052710f4a1d338ad18
FGavilasoto <fg.avilasoto@gmail.com>
2018-05-11 11:53:00 -0400

    Add files via upload

---
+++ IAmRich/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ferdinand Avila on 5/2/18.
+//  Copyright © 2018 Ferdinand Avila-Soto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
